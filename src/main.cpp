e5fcc74d84e09b2d72ea7629b4494b4ecfb05df6
Linnea Karlström <linnea.karlstrom@skola.botkyrka.se>
2020-09-03 11:19:46 +0200

    experiment

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include <Arduino.h>
 
 void setup() {
+  Serial.begin(9600);
+  Serial.println("Starting...!");
   // put your setup code here, to run once:
 }
 
 void loop() {
+  int value = analogRead(A0);
+  float voltage = 5.0 * value/1024;
+  Serial.println(voltage);
+  delay(1000);
   // put your main code here, to run repeatedly:
 }
